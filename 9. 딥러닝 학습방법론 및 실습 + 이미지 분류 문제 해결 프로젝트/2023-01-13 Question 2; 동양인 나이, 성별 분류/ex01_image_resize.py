e0e48889d2a0bf8e2542698f7b867d7a2d7ae833
ssyjgs1 <ssyjgs1@outlook.com>
2023-01-16 18:28:55 +0900

    edit path

--- 9. 딥러닝 학습방법론 및 실습 + 이미지 분류 문제 해결 프로젝트/2023-01-13 Question 2; 동양인 나이, 성별 분류/ex01_image_resize.py
+++ 9. 딥러닝 학습방법론 및 실습 + 이미지 분류 문제 해결 프로젝트/2023-01-13 Question 2; 동양인 나이, 성별 분류/ex01_image_resize.py
@@ -2,13 +2,10 @@ from PIL import Image # pip install Pillow
 import os
 import glob
 
-
 def padding(pil_img, set_size, background_color):
     width, height = pil_img.size
-
     w = set_size - width
     h = set_size - height
-
     if height != set_size & width != set_size :
         result = Image.new(pil_img.mode, (set_size, set_size), background_color)
         # image add(추가할 이미지, 붙일 위치(가로, 세로))
@@ -17,30 +14,16 @@ def padding(pil_img, set_size, background_color):
     else:
         return pil_img
 
-# def expand2square(pil_img, background_color) :
-#     width, height = pil_img.size   
-#     if width == height :
-#         return pil_img
-#     elif width > height :
-#         result = Image.new(pil_img.mode, (width, width), background_color)
-#         # image add(추가할 이미지, 붙일 위치(가로, 세로))
-#         result.paste(pil_img, (0, (width - height) // 2))
-#         return result
-#     else :
-#         result = Image.new(pil_img.mode, (height, height), background_color)
-#         result.paste(pil_img, ((height - width) // 2, 0))
-#         return result
-
-file_path = "./raw_data"   # 위치 자신의 파일경로에 맞게 변경 필요 !!!
+file_path = "./raw_test_data"   # 위치 자신의 파일경로에 맞게 변경 필요
 image_list = glob.glob(os.path.join(file_path, "*","*"))
 folder_list = glob.glob(os.path.join(file_path, "*"))
 for create_folder in folder_list:
-    create_folder= create_folder.replace("raw_data", "resize_data") 
+    create_folder = create_folder.replace("raw_test_data", "resize_test_data") 
     os.makedirs(create_folder, exist_ok=True) 
 
 for item_path in image_list:
     try:
-        newfile_path = item_path.replace("raw_data", "resize_data") # raw_data -> resize_data
+        newfile_path = item_path.replace("raw_test_data", "resize_test_data") # raw_data -> resize_data
         # 확장자 변환 (raw_data 확장자에 따라 수정 필요)
         newfile_path = newfile_path.replace(".jpg", ".png")         # png로 변환
         # newfile_path = newfile_path.replace(".jpeg", ".png")
