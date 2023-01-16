3fe7c31aa6465df509bd11957fe9756b258cff61
ssyjgs1 <ssyjgs1@outlook.com>
2023-01-16 18:26:55 +0900

    edit path

--- 9. 딥러닝 학습방법론 및 실습 + 이미지 분류 문제 해결 프로젝트/2023-01-13 Question 2; 동양인 나이, 성별 분류/ex00_create_label.py
+++ 9. 딥러닝 학습방법론 및 실습 + 이미지 분류 문제 해결 프로젝트/2023-01-13 Question 2; 동양인 나이, 성별 분류/ex00_create_label.py
@@ -8,25 +8,25 @@ CUT = 200
 
 # 라벨 폴더 만들기
 prev_path = "./previous_data"  # 경로 변경 필요 !!!
-raw_path = "./test_data"        # raw_data는 그대로 해주시고 raw_data 전 경로 변경 필요 !!!
+raw_path  = "./raw_data"        # raw_data는 그대로 해주시고 raw_data 전 경로 변경 필요 !!!
 os.makedirs(raw_path, exist_ok= True)
 
 # raw_data/20F ~ 60M 데이터 만들기
 for item in labels:
     os.makedirs(os.path.join(raw_path,item), exist_ok= True)
 
-# previous_data의 폴더 리스트 (15~75)
+# previous_data의 폴더 리스트 (20~75)
 folder_list = os.listdir(os.path.join(prev_path))
 switch_int_age = [int(t) for t in folder_list]  # str(15) -> int(15) 정수화
 # [20, 21, ...., 75]
 
-# 나이 20 ~ 75 까지
+# 나이 15 ~ 75 까지
 for age in switch_int_age:
     ##### 20대
     if (age >= TWENTY) & (age < THIRTY):
         ### 20대 여자      
         # 20폴더의 모든 여자 이미지(112), 용량 큰걸로 앞에서 1000장만 사용
-        image_list = sorted(glob.glob(os.path.join(prev_path,str(age), FEMALE, '*.jpg')), key=os.path.getsize, reverse=True)[CUT+1:CUT+11]  
+        image_list = sorted(glob.glob(os.path.join(prev_path,str(age), FEMALE, '*.jpg')), key=os.path.getsize, reverse=True)[:CUT]  
         try:
             for image_path in image_list:
                 image_name = image_path.split('\\')[-1].replace(".jpg", '.png')  # 100006-0.jpg -> 100006-0.png
@@ -38,7 +38,7 @@ for age in switch_int_age:
 
         ### 20대 남자   
         # 20폴더의 모든 남자 이미지(111), 용량 큰걸로 앞에서 1000장만 사용
-        image_list = sorted(glob.glob(os.path.join(prev_path,str(age), MALE, '*.jpg')), key=os.path.getsize, reverse=True)[CUT+1:CUT+11]  
+        image_list = sorted(glob.glob(os.path.join(prev_path,str(age), MALE, '*.jpg')), key=os.path.getsize, reverse=True)[:CUT] 
         try:
             for image_path in image_list:
                 image_name = image_path.split('\\')[-1].replace(".jpg", '.png') 
@@ -51,7 +51,7 @@ for age in switch_int_age:
     ##### 30대
     elif (age >= THIRTY) & (age < FOURTY):
         ### 30폴더의 모든 여자 이미지(112), 용량 큰걸로 앞에서 1000장만 사용 
-        image_list = sorted(glob.glob(os.path.join(prev_path,str(age), FEMALE, '*.jpg')), key=os.path.getsize, reverse=True)[CUT+1:CUT+11]  
+        image_list = sorted(glob.glob(os.path.join(prev_path,str(age), FEMALE, '*.jpg')), key=os.path.getsize, reverse=True)[:CUT] 
         try:
             for image_path in image_list:
                 image_name = image_path.split('\\')[-1].replace(".jpg", '.png') 
@@ -62,7 +62,7 @@ for age in switch_int_age:
             print(e)
 
         ### 30폴더의 모든 남자 이미지(111), 용량 큰걸로 앞에서 1000장만 사용
-        image_list = sorted(glob.glob(os.path.join(prev_path,str(age), MALE, '*.jpg')), key=os.path.getsize, reverse=True)[CUT+1:CUT+11]  
+        image_list = sorted(glob.glob(os.path.join(prev_path,str(age), MALE, '*.jpg')), key=os.path.getsize, reverse=True)[:CUT] 
         try:
             for image_path in image_list:
                 image_name = image_path.split('\\')[-1].replace(".jpg", '.png')  
@@ -71,7 +71,7 @@ for age in switch_int_age:
                 cv2.imwrite(new_path, image)     
         except Exception as e :
             print(e)
-"""
+
     ##### 40대
     elif (age >= FOURTY):
         ### 40대 여자      
@@ -95,4 +95,3 @@ for age in switch_int_age:
                 cv2.imwrite(new_path, image)    
         except Exception as e :
             print(e)
-"""
