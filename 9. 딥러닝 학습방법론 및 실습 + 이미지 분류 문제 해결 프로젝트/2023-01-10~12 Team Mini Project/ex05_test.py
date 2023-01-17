601708b05f243c538a9207360ce737e81ec489ce
ssyjgs1 <ssyjgs1@outlook.com>
2023-01-18 01:17:21 +0900

    edit script about confusion matrix, mobilenet test

--- 9. 딥러닝 학습방법론 및 실습 + 이미지 분류 문제 해결 프로젝트/2023-01-10~12 Team Mini Project/ex05_test.py
+++ 9. 딥러닝 학습방법론 및 실습 + 이미지 분류 문제 해결 프로젝트/2023-01-10~12 Team Mini Project/ex05_test.py
@@ -1,26 +1,24 @@
-import os
-import sys
 import torch    
 import torch.nn as nn
-import copy
 import matplotlib.pyplot as plt
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 from torch.utils.data import DataLoader
 from ex03_customdataset import CustomDataset
 import pandas as pd
-from tqdm import tqdm
 from torchvision import models
-from timm.loss import LabelSmoothingCrossEntropy
-import rexnetv1
-
+from ex04_main import FIX
+import numpy as np
+import seaborn as sn
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import rexnet
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-def main():
+def test_main():
     test_aug = A.Compose([
         A.SmallestMaxSize(max_size= 224),
-        A.Resize(width= 200, height= 200),
-        # A.CenterCrop(width= 180, height= 180),
+        A.CenterCrop(width= 200, height= 200),
         A.Normalize(mean=(0.485, 0.456, 0.406), std= (0.229, 0.224, 0.225)),
         ToTensorV2()
     ])
@@ -29,6 +27,11 @@ def main():
     test_loader  = DataLoader(test_dataset, batch_size= 1, shuffle= False, num_workers= 2, pin_memory= True)
 
     ###### 수정해야 할 부분 !!!!!!!!!!!!!!!!!!!
+    model = models.mobilenet_v2(pretrained= False)
+    model.classifier[1] = nn.Linear(in_features=1280, out_features= 2)
+    model.load_state_dict(torch.load(f"./12nd.pt", map_location=device))
+    model.to(device)
+
     # model = models.__dict__["resnet50"](pretrained= False)
     # model.fc = nn.Linear(in_features= 2048, out_features= 2)
     # model.load_state_dict(torch.load("./best0.pt", map_location=device))
@@ -44,11 +47,10 @@ def main():
     # model.load_state_dict(torch.load("./result/3rd.pt", map_location=device))
     # model.to(device)
 
-    model = rexnetv1.ReXNetV1()
-    model.output[1] = nn.Conv2d(1280, 2, kernel_size=1, stride=1)
-    model.load_state_dict(torch.load("./best0.pt"))
-    model.to(device)
-
+    # model = rexnetv1.ReXNetV1()
+    # model.output[1] = nn.Conv2d(1280, 2, kernel_size=1, stride=1)
+    # model.load_state_dict(torch.load("./best0.pt"))
+    # model.to(device)
 
     test(model, test_loader, device)
 
@@ -60,6 +62,7 @@ def test(model, data_loader, device) :
     model.eval()
     correct = 0
     total = 0
+    y_pred, y_true = [], []
     with torch.no_grad():
         for i, (image, label) in enumerate(data_loader) :
             images, labels = image.to(device), label.to(device)
@@ -67,8 +70,23 @@ def test(model, data_loader, device) :
             _, argmax = torch.max(output, 1)
             total += images.size(0)
             correct += (labels == argmax).sum().item()
+
+            argmax = argmax.data.cpu().numpy()  # gpu에 할당된 tensor를 cpu 텐서로 변환
+            labels = labels.data.cpu().numpy()  # gpu에 할당된 tensor를 cpu 텐서로 변환            
+
+            y_pred.extend(argmax) # Save Prediction
+            y_true.extend(labels) # Save True
+
         acc = acc_function(correct, total)
         print(f"acc >> {acc}%" )
 
+    # Build confusion matrix
+    classes = ('bird', 'drone')
+    cf_matrix = confusion_matrix(y_true, y_pred)
+    df_cm = pd.DataFrame(cf_matrix, index = [i for i in classes], columns = [i for i in classes])
+    plt.figure(figsize = (12,7))
+    sn.heatmap(df_cm, annot=True)
+    plt.savefig('./confunsion_matrix.png')    
+
 if __name__ == '__main__':
-    main()
+    test_main()
